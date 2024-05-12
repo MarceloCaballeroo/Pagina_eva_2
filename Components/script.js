067611e628725ee8fd4c296dc88a82a8f82192c2
MarceloCaballeroo <marc-caballero@duocuc.cl>
2024-05-12 00:12:12 -0400

    chore: Add form validation and submission logic to reservation form

--- Components/script.js
+++ Components/script.js
@@ -103,4 +103,30 @@ document.addEventListener("DOMContentLoaded", function() {
     .catch(error => console.warn(error));
 });
 
+$(document).ready(function() {
+  $('#reservationForm').submit(function(event) {
+  event.preventDefault();
+  var name = $('#name').val();
+  var email = $('#email').val();
+  var number = $('#number').val();
+  var phone = $('#phone').val();
+  var date = $('#date').val();
+  var time = $('#time').val();
 
+  if (name.trim() === '' || email.trim() === '' || number.trim() === '' || phone.trim() === '' || date.trim() === '' || time.trim() === '') {
+      alert('Por favor, completa todos los campos.');
+      return false;
+  }
+
+    // Validar el formato del correo electrónico
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+      alert('Por favor, introduce un correo electrónico válido.');
+      return false;
+  }
+
+    // Aquí puedes agregar el código para enviar el formulario si la validación es exitosa
+  alert('Formulario enviado con éxito.');
+    // Puedes agregar aquí el código para enviar el formulario mediante AJAX, por ejemplo
+  });
+});
